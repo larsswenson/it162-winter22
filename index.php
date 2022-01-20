04a32086935b89b79f2c12e3dd2922448fc51d20
larsswenson <94997829+larsswenson@users.noreply.github.com>
2022-01-19 16:14:26 -0800

    Upload of initial IT162 files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="Tlaquepaque" />
+ <img src="images/phone.jpg" class="phone" alt="Record Store" />
+ <p>Born and raised in Seattle, I grew up obsessed with music, specifically guitars and records. I'm also interested in travel, photography and cultural history.</p>
+ <p>Now in my second quarter at SCC, I'm enrolled in the IT program on an AAS-T track, seeking to transition away from the retail and record store work I'd previously been involved in for so many years.</p>
+ <p>I'm looking forward to the new challenges and opportunities my continuing education presents. I hope as I gain more knowledge and experience, specific areas of particular interest or ability will reveal themselves.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Quote Corner</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="With Willie" />
+ <p>"The point of optimism in life is not to avoid disappointment, rather what you learn from it."</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
